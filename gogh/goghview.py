3592482be1203e89cbc342503885c9a79fce8ea1
aleksey1977 <aleksey1977@07ecc120-1733-11de-b091-d144e675ee08>
2007-01-14 18:00:16 +0000

    Now can draw outside visible area when zoomed in

--- gogh/goghview.py
+++ gogh/goghview.py
@@ -58,23 +58,35 @@ class GoghView:
         if abs(self.zoom_factor-round(self.zoom_factor))<0.01:
             self.zoom_factor = round(self.zoom_factor)
         self.reset_view_pixbuf()
+        
+        
+    def scale_with_clipping(self, x, y, w, h):    
+        if x<0 :
+            w-=x
+            x=0            
+        if x+w>self.pixbuf0.get_width() :
+            w = self.pixbuf0.get_width()-x
+        if y<0 :
+            h-=y
+            y=0            
+        if y+h>self.pixbuf0.get_height() :
+            h = self.pixbuf0.get_height()-y
+        if w<=0 or h<=0 :
+            return
+        self.goghdoc.composite.scale(self.pixbuf0, x, y, w, h, -self.x_visible, -self.y_visible, self.zoom_factor, self.zoom_factor, gtk.gdk.INTERP_TILES)
        
     def update_view_pixbuf(self, x, y, w, h):
         xv, yv, wv, hv = self.to_view_rect(x, y, w, h)
-        self.goghdoc.composite.scale(self.pixbuf0, xv-self.x_visible, yv-self.y_visible, wv, hv, -self.x_visible, -self.y_visible, self.zoom_factor, self.zoom_factor, gtk.gdk.INTERP_TILES)
-        #self.goghdoc.composite.scale(self.pixbuf, xv, yv, wv, hv, 0, 0, self.zoom_factor, self.zoom_factor, gtk.gdk.INTERP_TILES) 
+        self.scale_with_clipping(xv-self.x_visible, yv-self.y_visible, wv, hv)
         
     def reset_view_pixbuf(self):
-        #w, h = self.get_size()
-        #self.pixbuf = self.goghdoc.composite.scale_simple(w, h, gtk.gdk.INTERP_TILES)
         if self.pixbuf0 :
             if (self.pixbuf0.get_width(), self.pixbuf0.get_height()) != (self.w_visible +1, self.h_visible+1):
                 del self.pixbuf0
                 self.pixbuf0 = create_pixbuf(self.w_visible +1, self.h_visible+1)
         else :
             self.pixbuf0 = create_pixbuf(self.w_visible +1, self.h_visible+1)
-        self.goghdoc.composite.scale(self.pixbuf0, 0, 0, self.w_visible +1, self.h_visible+1, -self.x_visible, -self.y_visible, self.zoom_factor, self.zoom_factor, gtk.gdk.INTERP_TILES)
-        #self.size_observer.notify_all()
+        self.scale_with_clipping(0, 0, self.w_visible +1, self.h_visible+1)
                 
     def refresh_area(self, area_rect=None):
         if not area_rect:
@@ -82,8 +94,7 @@ class GoghView:
             self.image_observer.notify_all(gtk.gdk.Rectangle(0, 0, self.w_visible, self.h_visible))
             return
         xv, yv, wv, hv = self.to_view_rect(area_rect.x, area_rect.y, area_rect.width, area_rect.height)
-        self.goghdoc.composite.scale(self.pixbuf0, xv-self.x_visible, yv-self.y_visible, wv, hv, -self.x_visible, -self.y_visible, self.zoom_factor, self.zoom_factor, gtk.gdk.INTERP_TILES) 
-        #self.goghdoc.composite.scale(self.pixbuf, xv, yv, wv, hv, 0, 0, self.zoom_factor, self.zoom_factor, gtk.gdk.INTERP_TILES) 
+        self.scale_with_clipping(xv-self.x_visible, yv-self.y_visible, wv, hv)
         self.image_observer.notify_all(gtk.gdk.Rectangle(xv, yv, wv, hv))
         
         
@@ -99,7 +110,6 @@ class GoghView:
             return            
         xv, yv, wv, hv = self.to_view_rect(x, y, w, h)
         self.drawable.draw_pixbuf(self.gc, self.pixbuf0, xv-self.x_visible, yv-self.y_visible, xv, yv, wv, hv) 
-        #self.drawable.draw_pixbuf(self.gc, self.pixbuf, xv, yv, xv, yv, wv, hv)      
         
     def to_view_rect(self, x, y, w, h):
         xv1, yv1 = map(lambda(t): int(floor(t)), self.to_view(x, y))
